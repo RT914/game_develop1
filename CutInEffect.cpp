e1acaa892c1eb49c90776c098f677797173cbd55
RT914 <trsub0914@outlook.jp>
2023-04-22 18:54:43 +0900

    add SpecialMove

---
+++ CutInEffect.cpp
@@ -0,0 +1,20 @@
+#include "Game.h"
+#include "CutInEffect.h"
+
+
+CutInEffect::CutInEffect(class Actor* actor, int drawOrder)
+    : SpriteComponent(actor, drawOrder)
+{
+    
+}
+
+void CutInEffect::Update(float deltatime)
+{
+
+}
+
+void CutInEffect::Draw(SDL_Renderer* renderer)
+{
+    
+
+}
--- CutInEffect.h
+++ CutInEffect.h
@@ -1,1 +1,25 @@
-#pragma once
+#ifndef _CUTINEFFECT_H_
+#define _CUTINEFFECT_H_
+
+#include "SpriteComponent.h"
+#include "Vector2.h"
+
+
+// アクタクラス
+// *各アクタはこのクラスを継承する
+class CutInEffect : public SpriteComponent
+{
+public:
+    CutInEffect(class Actor* actor, int drawOrder = 100);
+    void Update(float deltatime) override;
+    void Draw(SDL_Renderer* renderer) override;
+
+private:
+  
+
+public:
+   
+};
+
+
+#endif
